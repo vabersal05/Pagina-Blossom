1bea2023eb27e1468fd72082086323a1dc60253e
Valeria Chavez <valeria05p@outlook.com>
2026-04-14 16:10:41 -0600

    Icono carrito, Nosotros, Navbar

--- blossom-floristeria/src/app/pages/home/home.ts
+++ blossom-floristeria/src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,4 @@ import {RouterLink} from "@angular/router";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
- 
+export class Home {}
