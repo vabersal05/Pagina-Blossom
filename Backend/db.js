5d54194c46390f81e57d3032eebeb0a2bbc96d9c
Valeria Chavez <valeria05p@outlook.com>
2026-04-15 08:20:13 -0600

    resetForm

--- Backend/db.js
+++ Backend/db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error en la conexion a la BD', err);
-        return;
-    }
-    console.log('Conectado a la base de datos');
+connection.connect((err) => {
+  if (err) {
+    console.error("Error en la conexion a la BD", err);
+    return;
+  }
+  console.log("Conectado a la base de datos");
 });
 
 module.exports = connection;
